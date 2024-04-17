39bf774d01eab0820c2828400c9151223a697285
Gabriella Sirtoli Borges <77707319+GabriellaSBorges@users.noreply.github.com>
2024-04-17 09:17:56 -0300

    reading csv

--- src/App.js
+++ src/App.js
@@ -1,470 +1,91 @@
-import React from 'react';
-import { ResponsiveNetwork } from '@nivo/network'
-import './App.css';
-
-const Data = {
-  "nodes": [
-    {
-      "id": "Node 1",
-      "height": 1,
-      "size": 24,
-      "color": "rgb(97, 205, 187)"
-    },
-    {
-      "id": "Node 2",
-      "height": 1,
-      "size": 24,
-      "color": "rgb(97, 205, 187)"
-    },
-    {
-      "id": "Node 3",
-      "height": 1,
-      "size": 24,
-      "color": "rgb(97, 205, 187)"
-    },
-    {
-      "id": "Node 4",
-      "height": 1,
-      "size": 24,
-      "color": "rgb(97, 205, 187)"
-    },
-    {
-      "id": "Node 5",
-      "height": 1,
-      "size": 24,
-      "color": "rgb(97, 205, 187)"
-    },
-    {
-      "id": "Node 0",
-      "height": 2,
-      "size": 32,
-      "color": "rgb(244, 117, 96)"
-    },
-    {
-      "id": "Node 1.0",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 1.1",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 1.2",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 1.3",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 2.0",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 2.1",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 2.2",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 2.3",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 2.4",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 3.0",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 3.1",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 3.2",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 3.3",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 3.4",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 3.5",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.0",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.1",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.2",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.3",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.4",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.5",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.6",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.7",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 4.8",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.0",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.1",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.2",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.3",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.4",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.5",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.6",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    },
-    {
-      "id": "Node 5.7",
-      "height": 0,
-      "size": 12,
-      "color": "rgb(232, 193, 160)"
-    }
-  ],
-  "links": [
-    {
-      "source": "Node 0",
-      "target": "Node 1",
-      "distance": 80
-    },
-    {
-      "source": "Node 1",
-      "target": "Node 1.0",
-      "distance": 50
-    },
-    {
-      "source": "Node 1",
-      "target": "Node 1.1",
-      "distance": 50
-    },
-    {
-      "source": "Node 1",
-      "target": "Node 1.2",
-      "distance": 50
-    },
-    {
-      "source": "Node 1",
-      "target": "Node 1.3",
-      "distance": 50
-    },
-    {
-      "source": "Node 0",
-      "target": "Node 2",
-      "distance": 80
-    },
-    {
-      "source": "Node 2",
-      "target": "Node 2.0",
-      "distance": 50
-    },
-    {
-      "source": "Node 2",
-      "target": "Node 2.1",
-      "distance": 50
-    },
-    {
-      "source": "Node 2",
-      "target": "Node 2.2",
-      "distance": 50
-    },
-    {
-      "source": "Node 2",
-      "target": "Node 2.3",
-      "distance": 50
-    },
-    {
-      "source": "Node 2",
-      "target": "Node 2.4",
-      "distance": 50
-    },
-    {
-      "source": "Node 0",
-      "target": "Node 3",
-      "distance": 80
-    },
-    {
-      "source": "Node 3",
-      "target": "Node 3",
-      "distance": 80
-    },
-    {
-      "source": "Node 3",
-      "target": "Node 3.0",
-      "distance": 50
-    },
-    {
-      "source": "Node 3",
-      "target": "Node 3.1",
-      "distance": 50
-    },
-    {
-      "source": "Node 3",
-      "target": "Node 3.2",
-      "distance": 50
-    },
-    {
-      "source": "Node 3",
-      "target": "Node 3.3",
-      "distance": 50
-    },
-    {
-      "source": "Node 3",
-      "target": "Node 3.4",
-      "distance": 50
-    },
-    {
-      "source": "Node 3",
-      "target": "Node 3.5",
-      "distance": 50
-    },
-    {
-      "source": "Node 0",
-      "target": "Node 4",
-      "distance": 80
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.0",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.1",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.2",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.3",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.4",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.5",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.6",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.7",
-      "distance": 50
-    },
-    {
-      "source": "Node 4",
-      "target": "Node 4.8",
-      "distance": 50
-    },
-    {
-      "source": "Node 0",
-      "target": "Node 5",
-      "distance": 80
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.0",
-      "distance": 50
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.1",
-      "distance": 50
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.2",
-      "distance": 50
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.3",
-      "distance": 50
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.4",
-      "distance": 50
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.5",
-      "distance": 50
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.6",
-      "distance": 50
-    },
-    {
-      "source": "Node 5",
-      "target": "Node 5.7",
-      "distance": 50
-    }
-  ]
-};
-
-if(Data)  console.log(Data);
+import React, { useEffect, useState } from 'react';
+import { ResponsiveNetwork } from '@nivo/network';
 
 const MyResponsiveNetwork = ({ data }) => (
   <ResponsiveNetwork
-    data={data}
+    nodes={data.nodes}
+    links={data.links}
     margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-    linkDistance={e=>e.distance}
+    linkDistance={e => e.distance}
     centeringStrength={0.3}
     repulsivity={6}
-    nodeSize={n=>n.size}
-    activeNodeSize={n=>1.5*n.size}
-    nodeColor={e=>e.color}
+    nodeSize={n => n.size}
+    activeNodeSize={n => 1.5 * n.size}
+    nodeColor={e => e.color}
     nodeBorderWidth={1}
     nodeBorderColor={{
-        from: 'color',
-        modifiers: [
-            [
-                'darker',
-                0.8
-            ]
-        ]
+      from: 'color',
+      modifiers: [['darker', 0.8]]
     }}
-    linkThickness={n=>2+2*n.target.data.height}
+    linkThickness={n => 2 + 2 * n.target.data.height}
     linkBlendMode="multiply"
     motionConfig="wobbly" 
   />
 );
 
 export default function App() {
+  const [graphData, setGraphData] = useState(null);
+
+  useEffect(() => {
+    const readCSVFile = async (filePath) => {
+      try {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+          throw new Error("ERROR: file not found!");
+        }
+        const csvData = await response.text();
+        return csvData;
+      } catch (error) {
+        console.error("Error reading CSV file:", error);
+        return null;
+      }
+    };
+
+    const convertCSVDataToGraph = (csvData) => {
+      const parsedData = csvData.split('\n')
+        .slice(1) 
+        .map(row => {
+            row = row.replace('\r', '');
+            const [name, age, friend] = row.split(',');
+            return { name, age, friend };
+        });
   
+      const nodes = [];
+      const links = [];
+  
+      parsedData.forEach(row => {
+          const sourceNode = nodes.find(node => node.id === row.name);
+          const targetNode = nodes.find(node => node.id === row.friend);
+  
+          if (!sourceNode) nodes.push({ id: row.name });
+          if (!targetNode) nodes.push({ id: row.friend });
+          
+          const existingLink = links.find(link => (link.source === row.Node && link.target === row.Friend) || (link.source === row.Friend && link.target === row.Node));
+          if(!existingLink) links.push({ source: row.name, target: row.friend });
+      });
+  
+      return { nodes, links };
+    }
+
+    const csvFilePath = '/data/data.csv';
+    readCSVFile(csvFilePath)
+      .then(csvData => {
+        const graphData = convertCSVDataToGraph(csvData);
+        setGraphData(graphData);
+        
+        console.log(graphData)
+      });
+
+  }, []); 
+
+  if(!graphData)  console.log("NULOOO")
+
   return (
     <div>
-      <div className="container">
+      <div className="container" >
         <h1>GRAFO</h1>
-        <MyResponsiveNetwork data={Data}/>
+        {graphData && <MyResponsiveNetwork data={graphData}/>}
       </div>
     </div>
-    
   );
 }
-
